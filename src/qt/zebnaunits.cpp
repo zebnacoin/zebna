400e3a46aa778d9d0c3788d77c17c7ada55a3cae
Zebna Coin <zebnacoin@gmail.com>
2021-02-28 17:44:53 +0000

    updated file

--- src/qt/zebnaunits.cpp
+++ src/qt/zebnaunits.cpp
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <qt/ZebnaUnits.h>
+#include <qt/zebnaunits.h>
 
 #include <primitives/transaction.h>
 
